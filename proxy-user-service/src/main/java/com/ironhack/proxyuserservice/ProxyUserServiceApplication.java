dee25014e60ce6be3d8af44a58060f6ed278df4e
pablopggp <pablo.gutierrez.gp@gmail.com>
2021-10-09 12:26:53 +0200

    levantado modelo usuarios 2

--- proxy-user-service/src/main/java/com/ironhack/proxyuserservice/ProxyUserServiceApplication.java
+++ proxy-user-service/src/main/java/com/ironhack/proxyuserservice/ProxyUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.ironhack.proxyuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProxyUserServiceApplication {
 
 	public static void main(String[] args) {
