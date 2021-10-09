3260f4928fb866a4473d159d788cfaf2b1ff5544
almudena-ars <almudena.ars@gmail.com>
2021-10-09 11:04:25 +0200

    Added all services

---
+++ proxy-account-service/src/main/java/com/ironhack/proxyaccountservice/ProxyAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.proxyaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyAccountServiceApplication.class, args);
+	}
+
+}
---
+++ proxy-account-service/src/test/java/com/ironhack/proxyaccountservice/ProxyAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.proxyaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
